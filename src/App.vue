45b462bc7ef78efe5f4684d204c814721ec4807a
jordanyazzie <jordanyazzie@mail.weber.edu>
2018-10-09 16:56:22 -0600

    commits

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-Server></app-Server>
+            <app-ServerDetails></app-ServerDetails>
+        </div>
+        <hr>
+        <app-Footer></app-Footer>
+    </div>
+</template>
+
+<script>
+    import Header from './components/shared/Header.vue';
+    import Footer from './components/shared/Footer.vue';
+    import Server from './components/Server/Server.vue';
+    import ServerDetails from './components/Server/ServerDetails.vue';
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-Footer': Footer,
+            'app-Server': Server,
+            'app-ServerDetails': ServerDetails
+        }
+    }
+</script>
+
+<style>
+
+</style>
